A-Archives-and-Forks/The-Powder-Toy

commit d8da9ecc5f769de6587573bf79bee0fb876dc953

Fix soap movement in loop edge mode (closes #1036, closes #1038)
Also provides a low-effort fix to rendering them, but there are (literal) corner cases that are not handled; oh well.

src/graphics/Renderer.cpp
@@ -11,6 +11,7 @@
 #include "simulation/Air.h"
 #include "simulation/gravity/Gravity.h"
 #include "simulation/orbitalparts.h"
+#include "simulation/elements/SOAP.h"
 #include <algorithm>
 #include <cmath>
 #include <numbers>
@@ -503,7 +504,12 @@ void Renderer::render_parts()
 					if (t==PT_SOAP)
 					{
 						if ((parts[i].ctype&3) == 3 && parts[i].tmp >= 0 && parts[i].tmp < NPART)
-							BlendLine({ nx, ny }, { int(parts[parts[i].tmp].x+0.5f), int(parts[parts[i].tmp].y+0.5f) }, RGBA(colr, colg, colb, cola));
+						{
+							auto dx = parts[parts[i].tmp].x - nx;
+							auto dy = parts[parts[i].tmp].y - ny;
+							Element_SOAP_neighourLoop(dx, dy);
+							BlendLine({ nx, ny }, { int(nx + dx + 0.5f), int(ny + dy + 0.5f) }, RGBA(colr, colg, colb, cola));
+						}
 					}
 				}
 				if(pixel_mode & PSPEC_STICKMAN)

src/simulation/elements/SOAP.cpp
@@ -5,6 +5,14 @@ static int update(UPDATE_FUNC_ARGS);
 static int graphics(GRAPHICS_FUNC_ARGS);
 static void changeType(ELEMENT_CHANGETYPE_FUNC_ARGS);
 
+void Element_SOAP_neighourLoop(float &dx, float &dy)
+{
+	if (dx >= (XRES / 2 - CELL)) dx -= XRES - 2 * CELL;
+	if (dy >= (YRES / 2 - CELL)) dy -= YRES - 2 * CELL;
+	if (dx < -(XRES / 2 - CELL)) dx += XRES - 2 * CELL;
+	if (dy < -(YRES / 2 - CELL)) dy += YRES - 2 * CELL;
+}
+
 void Element::Element_SOAP()
 {
 	Identifier = "DEFAULT_PT_SOAP";
@@ -227,6 +235,7 @@ static int update(UPDATE_FUNC_ARGS)
 			float d, dx, dy;
 			dx = parts[i].x - parts[parts[i].tmp].x;
 			dy = parts[i].y - parts[parts[i].tmp].y;
+			Element_SOAP_neighourLoop(dx, dy);
 			d = 9/(pow(dx, 2)+pow(dy, 2)+9)-0.5;
 			parts[parts[i].tmp].vx -= dx*d;
 			parts[parts[i].tmp].vy -= dy*d;
@@ -241,6 +250,7 @@ static int update(UPDATE_FUNC_ARGS)
 				{
 					dx = parts[ii].x - parts[parts[i].tmp].x;
 					dy = parts[ii].y - parts[parts[i].tmp].y;
+					Element_SOAP_neighourLoop(dx, dy);
 					d = 81/(pow(dx, 2)+pow(dy, 2)+81)-0.5;
 					parts[parts[i].tmp].vx -= dx*d*0.5f;
 					parts[parts[i].tmp].vy -= dy*d*0.5f;

src/simulation/elements/SOAP.h
@@ -2,3 +2,4 @@
 #include "simulation/ElementDefs.h"
 
 void Element_SOAP_detach(Simulation * sim, int i);
+void Element_SOAP_neighourLoop(float &dx, float &dy);

commit 7f0bd1548b3ca178cd26e6a3ec847203fbf154ae

Fix SOAP looping in edge modes other than loop
Broken since d8da9ecc5f76, where, despite the commit message, I neglected gating the looping behaviour by whether the simulation is actually in loop edge mode.

src/graphics/Renderer.cpp
@@ -532,7 +532,7 @@ void Renderer::render_parts()
 						{
 							auto dx = parts[parts[i].tmp].x - nx;
 							auto dy = parts[parts[i].tmp].y - ny;
-							Element_SOAP_neighourLoop(dx, dy);
+							Element_SOAP_neighourLoop(sim, dx, dy);
 							BlendLine({ nx, ny }, { int(nx + dx + 0.5f), int(ny + dy + 0.5f) }, RGBA(colr, colg, colb, cola));
 						}
 					}

src/simulation/ElementDefs.h
@@ -40,6 +40,7 @@ constexpr auto FLAG_PHOTDECO      = UINT32_C(0x00000008); // compatibility with 
 
 class Parts;
 class Simulation;
+struct RenderableSimulation;
 struct GraphicsFuncContext;
 struct Particle;
 

src/simulation/Simulation.h
@@ -110,6 +110,7 @@ struct RenderableSimulation
 	int photons[YRES][XRES];
 
 	int aheat_enable = 0;
+	int edgeMode = EDGE_VOID;
 
 	bool useLuaCallbacks = false;
 };
@@ -151,7 +152,6 @@ public:
 
 	unsigned int pmap_count[YRES][XRES];
 
-	int edgeMode = EDGE_VOID;
 	int gravityMode = GRAV_VERTICAL;
 	float customGravityX = 0;
 	float customGravityY = 0;

src/simulation/elements/SOAP.cpp
@@ -5,12 +5,15 @@ static int update(UPDATE_FUNC_ARGS);
 static int graphics(GRAPHICS_FUNC_ARGS);
 static void changeType(ELEMENT_CHANGETYPE_FUNC_ARGS);
 
-void Element_SOAP_neighourLoop(float &dx, float &dy)
+void Element_SOAP_neighourLoop(const RenderableSimulation *sim, float &dx, float &dy)
 {
-	if (dx >= (XRES / 2 - CELL)) dx -= XRES - 2 * CELL;
-	if (dy >= (YRES / 2 - CELL)) dy -= YRES - 2 * CELL;
-	if (dx < -(XRES / 2 - CELL)) dx += XRES - 2 * CELL;
-	if (dy < -(YRES / 2 - CELL)) dy += YRES - 2 * CELL;
+	if (sim->edgeMode == EDGE_LOOP)
+	{
+		if (dx >= (XRES / 2 - CELL)) dx -= XRES - 2 * CELL;
+		if (dy >= (YRES / 2 - CELL)) dy -= YRES - 2 * CELL;
+		if (dx < -(XRES / 2 - CELL)) dx += XRES - 2 * CELL;
+		if (dy < -(YRES / 2 - CELL)) dy += YRES - 2 * CELL;
+	}
 }
 
 void Element::Element_SOAP()
@@ -235,7 +238,7 @@ static int update(UPDATE_FUNC_ARGS)
 			float d, dx, dy;
 			dx = parts[i].x - parts[parts[i].tmp].x;
 			dy = parts[i].y - parts[parts[i].tmp].y;
-			Element_SOAP_neighourLoop(dx, dy);
+			Element_SOAP_neighourLoop(sim, dx, dy);
 			d = 9/(pow(dx, 2)+pow(dy, 2)+9)-0.5;
 			parts[parts[i].tmp].vx -= dx*d;
 			parts[parts[i].tmp].vy -= dy*d;
@@ -250,7 +253,7 @@ static int update(UPDATE_FUNC_ARGS)
 				{
 					dx = parts[ii].x - parts[parts[i].tmp].x;
 					dy = parts[ii].y - parts[parts[i].tmp].y;
-					Element_SOAP_neighourLoop(dx, dy);
+					Element_SOAP_neighourLoop(sim, dx, dy);
 					d = 81/(pow(dx, 2)+pow(dy, 2)+81)-0.5;
 					parts[parts[i].tmp].vx -= dx*d*0.5f;
 					parts[parts[i].tmp].vy -= dy*d*0.5f;

src/simulation/elements/SOAP.h
@@ -2,4 +2,4 @@
 #include "simulation/ElementDefs.h"
 
 void Element_SOAP_detach(Simulation * sim, int i);
-void Element_SOAP_neighourLoop(float &dx, float &dy);
+void Element_SOAP_neighourLoop(const RenderableSimulation *sim, float &dx, float &dy);